0abf18011999eb556ab5aa288cb21415fd8cd0eb
Amar Mehta <xfthhxk@users.noreply.github.com>
2021-11-07 18:35:32 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# unminify
+unminifies JS stacktrace errors
+
+```shell
+# usage
+./unminify.cljs
+unminify: restore minified stacktrace using a source map
+
+  Usage:
+  unminifycljs :source-map '"index.js.map"' :stacktrace '"stacktrace.txt"'
+
+  Available options:
+  - :source-map (required) path to source map file
+  - :stacktrace (required) path to a file with the minified stacktrace
+```
+
+Example minified stacktrace:
+```shell
+cat stacktrace.txt
+TypeError: Cannot read properties of null (reading 'g')
+    at https://app.example.com/js/index.js:933:67
+    at tg (https://app.example.com/js/index.js:813:394)
+    at Function.bd.j (https://app.example.com/js/index.js:815:147)
+    at Function.ri.gb (https://app.example.com/js/index.js:933:34)
+    at https://app.example.com/js/index.js:3201:443
+    at https://app.example.com/js/index.js:1785:34
+    at i_ (https://app.example.com/js/index.js:1727:67)
+    at p_ (https://app.example.com/js/index.js:1741:67)
+    at o_ (https://app.example.com/js/index.js:1745:415)
+    at b (https://app.example.com/js/index.js:1724:150)
+```
+
+Unminify:
+```shell
+./unminifycljs :source-map '"index.js.map"' :stacktrace '"stacktrace.txt"'
+TypeError: Cannot read properties of null (reading 'g')
+    at f (cljs/core.cljs:5265:37)
+    at G__42694 (cljs/core.cljs:2527:20)
+    at cljs.core.iter_reduce (cljs/core.cljs:2577:8)
+    at cljs.core.reduce (cljs/core.cljs:5265:10)
+    at page (example/app/core.cljs:55:49)
+    at G__45346 (re_frame/subs.cljc:198:70)
+    at f (reagent/ratom.cljs:44:5)
+    at reagent.ratom/deref-capture (reagent/ratom.cljs:420:7)
+    at _run (reagent/ratom.cljs:415:6)
+    at reagent.impl.batching/ratom-flush (reagent/impl/batching.cljs:97:5)
+```
+
+## Built With
+* [nbb](https://github.com/babashka/nbb)
+
+
+## Credits
+* [Michiel Borkent](https://github.com/borkdude) for making nbb and other amazing tools so that I can Clojure all the things. Much joy!
+* [mifi](https://github.com/mifi) for [stacktracify](https://github.com/mifi/stacktracify)
+
+## License
+This is free and unencumbered software released into the public domain.
---
+++ unminify.cljs
@@ -0,0 +1,99 @@
+#!/usr/bin/env nbb
+;; Adapted from https://github.com/mifi/stacktracify
+(ns unminify
+  "unminify: restore minified stacktrace using a source map
+
+  Usage:
+  unminifycljs :source-map '\"index.js.map\"' :stacktrace '\"stacktrace.txt\"'
+
+  Available options:
+  - :source-map (required) path to source map file
+  - :stacktrace (required) path to a file with the minified stacktrace"
+  (:require ["stacktrace-parser" :as stacktrace-parser]
+            ["source-map" :as source-map]
+            ["fs" :as fs]
+            [clojure.edn :as edn]
+            [promesa.core :as p]))
+
+(defn unminify
+  [{:keys [source-map-file stacktrace]}]
+  (let [stack (->> (stacktrace-parser/parse stacktrace)
+                   (mapv (fn [obj]
+                           {:method-name (.-methodName obj)
+                            :line-number (.-lineNumber obj)
+                            :column (.-column obj)})))
+        src-map (-> (fs/readFileSync source-map-file "utf-8")
+                    (js/JSON.parse))
+        header (first (.split stacktrace "\n"))]
+    (p/let [consumer (source-map/SourceMapConsumer. src-map)]
+      (reduce (fn [ans {:keys [method-name line-number column]
+                       :or {method-name ""}}]
+                (if (or (not line-number) (< line-number 1))
+                  (conj ans {:method-name method-name})
+                  (let [p (.originalPositionFor
+                           consumer #js {:line line-number :column column})
+                        name (some-> p .-name)
+                        source (some-> p .-source)
+                        line (some-> p .-line)
+                        column (some-> p .-column)]
+                    (cond-> ans
+                      (and p line) (conj {:method-name name
+                                          :source source
+                                          :line line
+                                          :column column})))))
+              [{:header header}]
+              stack))))
+
+(defn stringify
+  [[header & stack]]
+  (reduce (fn [ans {:keys [method-name source line column]}]
+            (if line
+              (str ans "    at " method-name " (" source ":" line ":" column ")\n")
+              (str ans "    at " method-name "\n")))
+          (some-> header :header (str "\n"))
+          stack))
+
+(defn exit!
+  [status msg]
+  (println msg)
+  (js/process.exit status))
+
+(defn validate-args!
+  [{:keys [source-map stacktrace]}]
+  (when-not source-map
+    (exit! -1 ":source-map not specified"))
+  (when-not stacktrace
+    (exit! -1 ":stacktrace not specified"))
+  (when-not (fs/existsSync source-map)
+    (exit! -1 (str ":source-map file not found: " source-map)))
+  (when-not (fs/existsSync stacktrace)
+    (exit! -1 (str ":stacktrace file not found: " stacktrace))))
+
+(defn- usage
+  []
+  (-> *ns* meta :doc))
+
+
+(defn cli
+  []
+  ;; first 3 args are: node nbb filename
+  (let [args (drop 3 js/process.argv)
+        args-set (set args)]
+    (when (or (empty? args-set)
+              (contains? args-set "-h")
+              (contains? args-set "--help"))
+      (exit! -1 (usage)))
+    (when-not (even? (count args))
+      (exit! -1 "Expected even number of args"))
+    (let [{:keys [source-map
+                  stacktrace] :as args} (->> args
+                                             (mapv edn/read-string)
+                                             (apply hash-map))]
+      (validate-args! args)
+      (p/let [stack (unminify {:source-map-file source-map
+                               :stacktrace (fs/readFileSync stacktrace "utf-8")})]
+        (-> stack
+            stringify
+            println)))))
+
+(cli)
